aa19b3c3743798355d270fde7fc076a4b4938242
Adam <adamski01231@gmail.com>
2019-04-07 11:12:02 +0200

    Add base router.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const r = require('./web/routing/base.router');
 
 require('dotenv').config({
   path: path.join(__dirname, 'config', 'app.env')
@@ -9,6 +10,8 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT;
+
+app.use('/', r);
 
 app.listen(port, () => {
   console.log(`Server listenning on port ${port}`);
